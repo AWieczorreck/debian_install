ae9a58edff11717908399e0df899cad35e410d69
A. Wieczorreck <a.wieczorreck@tuta.io>
2026-03-05 00:00:04 +0100

    add install scripts for root and user

---
+++ install_root.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+su -
+USERNAME=""
+read -p "username: " USERNAME
+apt install -y sudo
+sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*\)"/\1 nosgx"/' /etc/default/grub
+echo "GRUB_GFXMODE=1920x1200" >> /etc/default/grub
+echo "GRUB_GFXPAYLOAD_LINUX=keep" >> /etc/default/grub
+usermod -a -G sudo $USERNAME
+update-grub
+echo "deb [arch=amd64,i386] http://deb.debian.org/debian trixie-backports main contrib non-free non-free-firmware" | tee /etc/apt/sources.list.d/trixie-backports.list
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); print "\n"$0"\n"}'
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
+apt update
+apt install -y -t trixie-backports linux-image-amd64 linux-headers-amd64 firmware-amd-graphics
+apt install -y build-essential libx11-dev libxft-dev libxinerama-dev xserver-xorg x11-xserver-utils x11-utils xinit libxcursor-dev libxcb1-dev libx11-xcb-dev libxcb-res0-dev xfonts-base xfonts-75dpi xfonts-100dpi xfonts-cyrillic gsfonts-x11 numlockx xserver-xorg-video-all xdotool xinput libgtk-3-dev libgcr-3-dev libwebkit2gtk-4.1-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev curl wget vim unzip pipewire acpi upower libfuse2t64 fuse libnss3-dev psmisc libopengl0 mc thunar lxpolkit dunst pavucontrol ffmpeg gstreamer1.0-libav gstreamer1.0-plugins-bad  gstreamer1.0-plugins-ugly npm bat ripgrep fzf fd-find gpg pcscd scdaemon pinentry-gtk2 pass yubikey-manager vulkan-tools mesa-vulkan-drivers firefox-nightly firefox-nightly-l10n-de power-profiles-daemon ckb-next spotify-client rdfind gamemode
+systemctl enable upower.service
+systemctl start upower.service
+echo 'KERNEL=="vga_arbiter", GROUP="video", MODE="0660"' | tee /etc/udev/rules.d/99-vga-arbiter.rules
+chmod u+s /usr/bin/Xorg
+sed -i 's/^XKBOPTIONS=""/#XKBOPTIONS=""/' /etc/default/keyboard
+sed -i '$i\ \ \ \ \ \ \ \ Option "TearFree" "true"' /usr/share/X11/xorg.conf.d/10-amdgpu.conf
+sed -i '$i\ \ \ \ \ \ \ \ Option "SWCursor" "true"' /usr/share/X11/xorg.conf.d/10-amdgpu.conf
+sed -i '$i\ \ \ \ \ \ \ \ Option "DRI" "3"' /usr/share/X11/xorg.conf.d/10-amdgpu.conf
+sed -i '/HotplugDriver/d' /usr/share/X11/xorg.conf.d/10-amdgpu.conf
+cd /etc/fonts/conf.d && rm -f 70-no-bitmaps*.conf && ln -s ../conf.avail/70-yes-bitmaps.conf
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git /home/$USERNAME/repos/linux-firmware
+cd /home/$USERNAME/repos/linux-firmware
+chown -R $USERNAME:$USERNAME /home/$USERNAME/repos/linux-firmware/
+make install
+make dedup
+update-initramfs -u -k all
+git clone https://github.com/vim/vim.git /home/$USERNAME/repos/vim
+cd /home/$USERNAME/repos/vim
+chown -R $USERNAME:$USERNAME /home/$USERNAME/repos/vim/
+./configure --with-features=huge --with-x --prefix=/usr/local
+make install
+sed -i 's/^# \(en_US.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/^# \(de_DE.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+cp /home/$USERNAME/repos/debian_install/tools/ckb-next-daemon.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable ckb-next-daemon
+systemctl start ckb-next-daemon
+wget https://gist.githubusercontent.com/archisman-panigrahi/cd571ddea1aa2c5e2b4fa7bcbee7d5df/raw/setup-snixembed-debian.sh && bash setup-snixembed-debian.sh
+echo "RADV_PERFTEST=aco" | tee -a /etc/environment
+sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*\)"/\1 amdgpu.dc=1"/' /etc/default/grub
+groupadd gamemode
+usermod -a -G gamemode $USERNAME
+curl -L -o uw-ttyp0.tar.gz "https://people.mpi-inf.mpg.de/~uwe/misc/uw-ttyp0/uw-ttyp0-2.1.tar.gz"
+tar xf uw-ttyp0.tar.gz && cd uw-ttyp0-2.1 && ./configure && make && make install
+cd
+rm ~/uw-ttyp0.tar.gz
+rm -rf ~/uw-ttyp0-2.1
---
+++ install_user.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+GH_USERNAME=""
+read -p "github username" GH_USERNAME
+cp ~/repos/debian_install/tools/passmenu /usr/local/bin
+sudo cp ~/repos/debian_install/tools/totpmenu /usr/local/bin
+cp ~/repos/debian_install/tools/startdwm/.xinitrc ~
+TOOL=dwm
+git clone https://git.suckless.org/$TOOL ~/repos/$TOOL
+cd ~/repos/$TOOL && rm -f config.h && patch -i ~/repos/debian_install/$TOOL/$(echo $TOOL)_patch.diff && sudo make install
+TOOL=st
+git clone https://git.suckless.org/$TOOL ~/repos/$TOOL
+cd ~/repos/$TOOL && rm -f config.h && patch -i ~/repos/debian_install/$TOOL/$(echo $TOOL)_patch.diff && sudo make install
+TOOL=surf
+git clone https://git.suckless.org/$TOOL ~/repos/$TOOL
+cd ~/repos/$TOOL && rm -f config.h && patch -i ~/repos/debian_install/$TOOL/$(echo $TOOL)_patch.diff && sudo make install
+TOOL=dmenu
+git clone https://git.suckless.org/$TOOL ~/repos/$TOOL
+cd ~/repos/$TOOL && rm -f config.h && sudo make install
+TOOL=slstatus
+git clone https://git.suckless.org/$TOOL ~/repos/$TOOL
+cd ~/repos/$TOOL && rm -f config.h && patch -i ~/repos/debian_install/$TOOL/$(echo $TOOL)_patch.diff && sudo make install
+git clone https://github.com/$GH_USERNAME/dotfiles ~/repos/dotfiles
+cp -r ~/repos/dotfiles/.gnupg ~
+chmod 700 ~/.gnupg/ && chmod 700 ~/.gnupg/*
+echo "export PASSWORD_STORE_GPG_OPTS='--no-throw-keyids'" >> .bashrc
+printf "%s\n" "export GPG_TTY=\$(tty)" "gpg-connect-agent updatestartuptty /bye > /dev/null" >> .bashrc
+printf "fetch\nquit\n" | script -q -c "gpg --card-edit" /dev/null
+EMAIL=""
+read -p "gpg email: " EMAIL
+printf "trust\n5\nj\nquit\n" | script -q -c "gpg --key-edit $EMAIL" /dev/null
+git clone git@github.com:$GH_USERNAME/password-store ~/.password-store
+curl -sSL https://codeberg.org/PassFF/passff-host/releases/download/latest/install_host_app.sh | bash -s -- firefox
+git config --global user.name "$(gpg --with-colons -K | grep '^uid:' | cut -d: -f10 | sed 's/ *<.*>//')"
+git config --global user.email $(gpg --with-colons -K | grep '^uid:' | cut -d: -f10 | sed -n 's/.*<\([^>]*\)>.*/\1/p')
+git config --global user.signingkey $(gpg --with-colons -K | awk -F: '$1=="ssb" && $12 ~ /S/ {print $5; exit} $1=="sec"{p=$5} END{if(!NR || !p){} else if(!seen && !system("")) print p}')
+git config --global init.defaultBranch main
+mkdir -p ~/.local/share/fonts/
+curl -L -o Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
+unzip Hack.zip -d ~/.local/share/fonts/Hack/
+fc-cache -fv
