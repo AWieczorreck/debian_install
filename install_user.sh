2aa74e64a2501a7c071603f46e1ff610c0b19d88
A. Wieczorreck <a.wieczorreck@tuta.io>
2026-03-05 23:52:58 +0100

    change step orders and set sudo permission

--- install_user.sh
+++ install_user.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 GH_USERNAME=""
+EMAIL=""
 read -p "github username: " GH_USERNAME
-cp ~/repos/debian_install/tools/passmenu /usr/local/bin
+read -p "gpg email: " EMAIL
+sudo cp ~/repos/debian_install/tools/passmenu /usr/local/bin
 sudo cp ~/repos/debian_install/tools/totpmenu /usr/local/bin
 echo "export QT_QPA_PLATFORMTHEME=qt5ct" >> ~/.profile
 cp ~/repos/debian_install/tools/startdwm/.xinitrc ~
@@ -26,11 +28,7 @@ chmod 700 ~/.gnupg/ && chmod 700 ~/.gnupg/*
 echo "export PASSWORD_STORE_GPG_OPTS='--no-throw-keyids'" >> ~/.bashrc
 printf "%s\n" "export GPG_TTY=\$(tty)" "gpg-connect-agent updatestartuptty /bye > /dev/null" >> ~/.bashrc
 printf "fetch\nquit\n" | script -q -c "gpg --card-edit" /dev/null
-EMAIL=""
-read -p "gpg email: " EMAIL
 printf "trust\n5\nj\nquit\n" | script -q -c "gpg --key-edit $EMAIL" /dev/null
-git clone git@github.com:$GH_USERNAME/password-store ~/.password-store
-curl -sSL https://codeberg.org/PassFF/passff-host/releases/download/latest/install_host_app.sh | bash -s -- firefox
 git config --global user.name "$(gpg --with-colons -K | grep '^uid:' | cut -d: -f10 | sed 's/ *<.*>//')"
 git config --global user.email $(gpg --with-colons -K | grep '^uid:' | cut -d: -f10 | sed -n 's/.*<\([^>]*\)>.*/\1/p')
 git config --global user.signingkey $(gpg --with-colons -K | awk -F: '$1=="ssb" && $12 ~ /S/ {print $5; exit} $1=="sec"{p=$5} END{if(!NR || !p){} else if(!seen && !system("")) print p}')
@@ -41,3 +39,5 @@ unzip Hack.zip -d ~/.local/share/fonts/Hack/
 fc-cache -fv
 cd ~/repos/debian_install
 git remote set-url origin git@github.com:$GH_USERNAME/debian_install
+git clone git@github.com:$GH_USERNAME/password-store ~/.password-store
+curl -sSL https://codeberg.org/PassFF/passff-host/releases/download/latest/install_host_app.sh | bash -s -- firefox
