b77e470a3a5af9a233d5b4f170c7c3c2ad4fbc24
A. Wieczorreck <a.wieczorreck@tuta.io>
2026-03-09 20:58:32 +0100

    formatting

--- setup_root.sh
+++ setup_root.sh
@@ -157,9 +157,9 @@ install_xorg() {
         xfonts-100dpi \
         xfonts-cyrillic \
         gsfonts-x11 \
-	fonts-noto \
-	fonts-noto-cjk \
-	fonts-noto-extra \
+        fonts-noto \
+        fonts-noto-cjk \
+        fonts-noto-extra \
         numlockx \
         xdotool \
         xinput \
